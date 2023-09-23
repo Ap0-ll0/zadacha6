ac426ef8dd59ed30d969876993c8517ad93916d9
Apollo <polinavestel@gmail.com>
2023-09-23 18:50:18 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main (args: Array<String>) {
+    val str1 = readln()
+    print("Введите второе число: ")
+    val str2 = readln()
+    println("Нечетные числа, созданные из двух чисел: " + zadacha6(str1, str2))
+}
+fun zadacha6(a: String, b: String): String {
+    var str1: String? = null
+    var str2: String? = null
+    if ("$a$b".toInt() % 2 != 0) {
+        str1 = "$a$b"
+    }
+    if ("$b$a".toInt() % 2 != 0) {
+        str2 = "$b$a"
+    }
+    var res = ""
+    if (str1 == null && str2 == null) return "Создать нечетное число невозможно"
+    if (str1 != null) res += str1
+    if (str2 != null) {
+        if (str1 != null) res += " "
+        res += str2
+    }
+    return res
+}
